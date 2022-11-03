29a0faaacb35a537a202ddc3be05f89590fdef3f
Thomas Kim <kimsh92@hotmail.com>
2022-11-03 13:42:08 +0900

    springboot init
    springboot init

---
+++ src/test/java/com/oracle/ocidevopsdemo/OciDevopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.ocidevopsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OciDevopsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
